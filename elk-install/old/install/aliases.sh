3da2126cb19d20e1f87b528f0b68783bfd7ac1f2
dgowran <dgowran@informatica.com>
2020-08-14 23:43:06 +0100

    update

--- elk-install/old/install/aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -vx
-
-alias k="kubectl"
-alias kgp="kubectl get pods"
-alias kgps="kubectl get pods --namespace=kube-system"
-alias kd="kubectl delete -f"
-alias kds="kubectl delete --namespace=kube-system -f"
-alias kc="kubectl create -f"
-alias kcs="kubectl create --namespace=kube-system -f"
